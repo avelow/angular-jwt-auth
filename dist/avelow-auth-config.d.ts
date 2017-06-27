5bed4bce26d20628e2ec6d6f52d8554766ef32ae
Alexis Merceron <merceron.alexis.dev@gmail.com>
2017-06-27 21:02:40 +0200

    update gitignore

--- dist/avelow-auth-config.d.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-export declare let AVELOW_AUTH_CONFIG: InjectionToken<AvelowAuthConfig>;
-export interface AvelowAuthConfig {
-    loginUrl: string;
-    apiUrl: string;
-    tokenName: string;
-    usernameParam: string;
-    passwordParam: string;
-}
